2952f4ed59694e17e307b7e7af6aa92e8e82b344
marcodomenichetti <marcodomenichetti@gmail.com>
2017-10-26 15:46:01 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='prova10',
+      version='0.1',
+      description='prova10',
+      url='https://github.com/marcodomenichetti/prova10.git',
+      author='marco domenichetti',
+      author_email='marcodomenichetti@gmail.com',
+      license='MIT',
+      packages=['prova10'],
+      zip_safe=False)
